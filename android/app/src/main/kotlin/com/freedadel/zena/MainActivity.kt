4db7e8b566aba52b073a20e983246473f5495066
freed adel <freedoadel@hotmail.com>
2022-05-16 13:16:25 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/freedadel/zena/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.freedadel.zena
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utill/styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const poppinsLight = TextStyle(
+  fontFamily: 'Questv1',
+  fontWeight: FontWeight.w300,
+);
+
+const poppinsRegular = TextStyle(
+  fontFamily: 'Questv1',
+  fontWeight: FontWeight.w400,
+);
+
+const poppinsMedium = TextStyle(
+  fontFamily: 'Questv1',
+  fontWeight: FontWeight.w500,
+);
+
+const poppinsSemiBold = TextStyle(
+  fontFamily: 'Questv1',
+  fontWeight: FontWeight.w600,
+);
+
+const poppinsBold = TextStyle(
+  fontFamily: 'Sukar',
+  fontWeight: FontWeight.w700,
+);
